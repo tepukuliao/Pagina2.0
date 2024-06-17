09e006d3d06d9fe57adb51ebb890d11aba4744ce
Vina del Mar <vina@duoc.acad>
2024-06-17 10:26:24 -0400

    el fin se acerca

--- tienda_web/main/views.py
+++ tienda_web/main/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.db import IntegrityError
+import requests
 
 
 def index(request):
@@ -13,13 +14,14 @@ def formulario(request):
     try:
         if request.method == "POST":
             nombre = request.POST.get("nombre") 
+            apellido = request.POST.get("apellido")
             usuario = request.POST.get("usuario")
             correo = request.POST.get("correo")
             password1 = request.POST.get("password1")
             password2 = request.POST.get("password2")
-            print(nombre,usuario,correo,password1,password2)
+            print(nombre,apellido,usuario,correo,password1,password2)
             if password1 == password2:
-                user = User.objects.create_user(first_name=nombre, username=usuario, email=correo, password=password1)
+                user = User.objects.create_user(first_name=nombre, last_name=apellido, username=usuario, email=correo, password=password1)
                 user.save()
                 return render(request,'login.html')
             else:
@@ -54,3 +56,9 @@ def iniciar_sesion(request):
 
 def perfil(request):
     return render(request,'perfil.html')
+
+
+def home(request):
+    url = url
+    response = requests.get(url)
+    
